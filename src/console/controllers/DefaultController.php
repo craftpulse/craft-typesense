0de5e13d050697c5ee70d53ef52c8f284e33ec33
Michael Thomas <michael@dadamotion.be>
2022-01-24 12:50:10 +0100

    fix: fixed typo

--- src/console/controllers/DefaultController.php
+++ src/console/controllers/DefaultController.php
@@ -58,7 +58,7 @@ class DefaultController extends Controller
     {
         $result = 'something';
 
-        echo "Welcome to the console CollectionController actionIndex() method\n";
+        echo "Welcome to the console CollectionsController actionIndex() method\n";
 
         return $result;
     }
@@ -75,7 +75,7 @@ class DefaultController extends Controller
     {
         $result = 'something';
 
-        echo "Welcome to the console CollectionController actionDoSomething() method\n";
+        echo "Welcome to the console CollectionsController actionDoSomething() method\n";
 
         return $result;
     }
