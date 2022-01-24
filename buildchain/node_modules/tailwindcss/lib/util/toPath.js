a2f3503fa1c26de66dfb327f4eeb4de597a37e57
Michael Thomas <michael@dadamotion.be>
2022-01-24 12:57:35 +0100

    chore: git cache rebuild

--- buildchain/node_modules/tailwindcss/lib/util/toPath.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.toPath = toPath;
-function toPath(path) {
-    if (Array.isArray(path)) return path;
-    return path.split(/[\.\]\[]+/g);
-}
