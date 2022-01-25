f9c977f4f5cf83389846f7ba4e7c8f5721065c74
Michael Thomas <michael@dadamotion.be>
2022-01-25 16:14:06 +0100

    feat: created collection event

--- src/events/CollectionEvent.php
+++ src/events/CollectionEvent.php
@@ -2,7 +2,23 @@
 
 namespace percipiolondon\typesense\events;
 
-class CollectionEvent
-{
+use yii\base\Event;
 
+/**
+ * Collection event class.
+ *
+ * @author Percipio Global Ltd. <support@percipio.london>
+ * @since 1.0.0
+ */
+class CollectionEvent extends Event
+{
+    /**
+     * @var \percipiolondon\models\Collection|null The collection model associated with the event.
+     */
+    public $collection;
+
+    /**
+     * @var bool Whether the collection is brand new
+     */
+    public $isNew = false;
 }
