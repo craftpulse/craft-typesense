8df582fbe603675ab3b276122d14067da9d308c6
Michael Thomas <michael@dadamotion.be>
2022-01-25 16:18:03 +0100

    feat: created Service Loader

--- src/typesense/Services.php
+++ src/typesense/Services.php
@@ -2,7 +2,26 @@
 
 namespace percipiolondon\typesense\typesense;
 
-class Services
-{
+use percipiolondon\typesense\services\CollectionService as Collections;
 
+/**
+ * Trait Services
+ *
+ * @property Collections $collections the collection service
+
+ * @author Percipio Global Ltd. <support@percipio.london>
+ * @since 1.0.0
+ */
+
+trait Services
+{
+    /**
+     * Returns the collection service
+     *
+     * @return Collections The collection service
+     */
+    public function getCollections(): Collections
+    {
+        return $this->get('collections');
+    }
 }
