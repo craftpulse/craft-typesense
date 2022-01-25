07f78f6645799aa4fdaf9724a1c15d969117c3c1
Michael Thomas <michael@dadamotion.be>
2022-01-25 16:15:07 +0100

    feat: created Project Config Data helper

--- src/helpers/ProjectConfigDataHelper.php
+++ src/helpers/ProjectConfigDataHelper.php
@@ -2,7 +2,35 @@
 
 namespace percipiolondon\typesense\helpers;
 
+use Craft;
+use craft\typesense\db\Table;
+use craft\typesense\Typesense;
+use craft\db\Query;
+use craft\helpers\Json;
+
 class ProjectConfigDataHelper
 {
+    /**
+     * Return a rebuilt project config array
+     *
+     * @return array
+     */
+    public static function rebuildProjectConfig(): array
+    {
+        $output = self::_getCollectionData();
+    }
 
+    /**
+     * Return collection data config array.
+     *
+     * @return array
+     */
+    private static function _getCollectionData(): array
+    {
+        $data = [];
+        foreach (Typesense::getInstance()->getCollection()->getAllCollections() as $collection) {
+            $data[$collection->uid] = $collection->getConfig();
+        }
+        return $data;
+    }
 }
