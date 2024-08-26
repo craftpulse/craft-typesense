9b2bfe9757ef513d93f24681b9f1023a3723c4a5
Michael Thomas <michael@dadamotion.be>
2024-08-26 18:41:11 +0200

    feat: #35 allow multiple indexes

--- src/helpers/CollectionHelper.php
+++ src/helpers/CollectionHelper.php
@@ -38,7 +38,7 @@ class CollectionHelper
         $indexes = Typesense::$plugin->getSettings()->collections;
 
         foreach ($indexes as $index) {
-            if ($index->section === $name) {
+            if ($index->section === $name || (is_array($index->section) && in_array($name, $index->section))) {
                 return $index;
             }
         }
