a28e6ddb896f4f3ca80539f35a6fb79f12da80f5
Mark <menm98@live.nl>
2025-02-19 10:53:08 +0100

    feat: added category to the elementTypes

--- src/controllers/SynonymController.php
+++ src/controllers/SynonymController.php
@@ -75,6 +75,21 @@ class SynonymController extends Controller
                     }
                     break;
 
+                case 'craft\elements\Category':
+                    $group = $element->group ?? null;
+
+                    if ($group) {
+                        $variables['sections'][] = [
+                            'id' => $group->id,
+                            'name' => $group->name,
+                            'handle' => $group->handle,
+                            'type' => 'Category: ' . $element->group->handle,
+                            'entryCount' => $index->criteria->count(),
+                            'index' => $index->indexName,
+                        ];
+                    }
+                    break;
+
                 case 'craft\commerce\elements\Product':
                     $type = $element->type ?? null;
                     if ($type) {
